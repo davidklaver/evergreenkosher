071feb3882d6a268ea45a4ebec821dd0ccd73757
David Klaver <davidjklaver@gmail.com>
2018-08-23 13:00:33 -0400

    styling on donations page

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -8,10 +8,12 @@ $(window).bind('scroll', function() {
         var navHeight = $(window).height() - 700;
         if ($(window).scrollTop() > navHeight) {
             $('#menu').addClass('on');
+            $('#menu').addClass('shorter');
             $('.page-scroll').addClass('whiteText');
             $('.navLogo').addClass('visible');
         } else {
             $('#menu').removeClass('on');
+            $('#menu').removeClass('shorter');
             $('.page-scroll').removeClass('whiteText');
             $('.navLogo').removeClass('visible');
         }
